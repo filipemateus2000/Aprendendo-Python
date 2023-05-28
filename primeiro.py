5b4285886fb4ec8711890c572c62f8494edf3d0d
filipemateus2000 <filipemateus2000@hotmail.com>
2023-05-28 00:51:12 -0300

    Aula de Variáveis

--- primeiro.py
+++ primeiro.py
@@ -1,4 +1,13 @@
+# PRIMEIRO CÓDIGO
+
 print('Hello World!')
 print('Seja bem-vindo(a) ao curso de Python da Coding Tank!')
 
 input('Qual a linguagem de Programação que você está estudando! ')
+
+# Adição de Comentário para uma linha
+
+"""
+Adição de Comentário, para mais de tuma linha é TRÊS ASPAS
+
+"""
---
+++ variáveis.py
@@ -0,0 +1,53 @@
+# > AULA DE VARIÁVEIS
+
+# 1. Variáveis
+
+#idade = 22 #criando uma variável
+
+#print(idade)
+
+#nome = 'Filipe Mateus'
+
+#print(nome)
+
+"""
+    TIPOS DE VARIÁVEIS
+
+1. int: números inteiros, ou seja, números sem parte decimal: 0, 5, -1, 1000
+
+2. float: números reais, ou seja, números com parte decimal: 1.0, -2.7, 3.14
+
+3. str: cadeias de caracteres, ou seja, dados textuais (string)
+
+4. bool: valores lógicos (booleanos): True ou false
+
+"""
+
+"""""
+
+--> TESTE DAS VARIÁVEIS <--
+
+idade = 23
+altura = 1.70,
+nome = 'Filipe Mateus'
+estudando = True
+
+print(type(idade))
+print(type(altura))
+print(type(nome))
+print(type(estudando))
+
+"""
+
+
+
+# OBTENDO DADOS DO USUÁRIO E SALVANDO EM VARIÁVEIS
+
+linguagem = input('Qual é a linguagem de programação que você está estudando?')
+
+print('A linguagem que você esta estudando é', linguagem)
+ 
+
+
+# Imprimindo variáveis + Mais sobre + função print
+
